25b3d2e4e42c04ce627757bf566e7c97232da0e6
Giroum Tadrous <giroum_tadrous@aucegypt.edu>
2026-08-16 10:56:10 +0300

    updatess

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -3,13 +3,12 @@ set -e
 
 cd "$CI_PRIMARY_REPOSITORY_PATH"
 
-# Install Flutter (adjust the branch/version to match what your app uses)
 git clone https://github.com/flutter/flutter.git --depth 1 -b stable $HOME/flutter
 export PATH="$PATH:$HOME/flutter/bin"
 
+flutter config --no-enable-swift-package-manager
 flutter doctor
 flutter pub get
-cat .flutter-plugins-dependencies
 flutter precache --ios
 
 cd ios
