0ba93d508dacdcf240e9914c26df784c07787475
Giroum Tadrous <giroum_tadrous@aucegypt.edu>
2026-08-16 10:19:41 +0300

    Add ci_post_clone.sh for Xcode Cloud

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-cd $CI_WORKSPACE
+cd "$CI_PRIMARY_REPOSITORY_PATH"
 
 # Install Flutter (adjust the branch/version to match what your app uses)
 git clone https://github.com/flutter/flutter.git --depth 1 -b stable $HOME/flutter
