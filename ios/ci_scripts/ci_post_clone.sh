bcab1823dfa3dc702a40d146f8329d9a8b3544b8
Giroum Tadrous <giroum_tadrous@aucegypt.edu>
2026-09-20 17:07:11 +0300

    Edits for not crashing in iOS

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -12,4 +12,26 @@ flutter pub get
 flutter precache --ios
 
 cd ios
+
+ruby -e '
+require "xcodeproj"
+
+project_path = "Runner.xcodeproj"
+project = Xcodeproj::Project.open(project_path)
+target = project.targets.find { |t| t.name == "Runner" }
+group = project.main_group.find_subpath("Runner", true)
+
+file_name = "SceneDelegate.swift"
+existing = target.source_build_phase.files_references.any? { |f| f.path == file_name }
+
+unless existing
+  file_ref = group.new_reference(file_name)
+  target.add_file_references([file_ref])
+  project.save
+  puts "Added #{file_name} to Runner target"
+else
+  puts "#{file_name} already in Runner target"
+end
+'
+
 pod install
